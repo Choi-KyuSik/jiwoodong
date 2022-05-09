567de0f2b24962bf5a2f837792ba8dac5c163461
XeonSeunghee <kingbambbang2@gmail.com>
2022-05-09 10:01:17 +0900

    New_review

--- jiwoodong_project/src/kh/semi/jwd/bum/model/service/BumService.java
+++ jiwoodong_project/src/kh/semi/jwd/bum/model/service/BumService.java
@@ -153,4 +153,28 @@ public class BumService {
 			close(conn);
 			return list;
 		}
+	
+	
+	
+	
+	
+	
+	
+	// 승희 검색 조건에 맞는 게시물 목록을 반환합니다.
+		public List<ReviewVo> selectRvList(Map<String, Object> map){
+			Connection conn = getConnection();
+			List<ReviewVo> volist = new BumDao().selectRvList(map);
+			close(conn);
+			System.out.println("BumService selectRvList volist:"+volist);
+			return volist;
+		}
+	//승희 리뷰상세페이지
+		public ReviewVo detailRv(ReviewVo vo) {
+			Connection conn = getConnection();
+			ReviewVo rvo = new BumDao().detailRv(conn, vo);
+			close(conn);
+			System.out.println("BumService datailRv() ReviewVo rvo :" + rvo);
+			return rvo;
+		}
+
 }
